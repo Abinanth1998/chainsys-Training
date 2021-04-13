c28d92b4191a4379d3a7f422c2345a4a8906b03d
Abinanth1998 <69683908+Abinanth1998@users.noreply.github.com>
2021-04-13 17:47:18 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Addition {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		
+		int num1=scan.nextInt();
+		int num2=scan.nextInt();
+		int answer=num1+num2;
+		scan.close();
+		System.out.println(answer);
+
+	}
+
+}
