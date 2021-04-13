7d9f049c7e3cb9deb515f0ba01faccf2630b030d
Abinanth1998 <69683908+Abinanth1998@users.noreply.github.com>
2021-04-13 17:49:44 +0530

    Add files via upload

---
+++ UsingString.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class UsingString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scan=new Scanner(System.in);
+String name1=scan.next();
+String name2=scan.next();
+scan.close();
+System.out.println(name1+name2);
+	}
+
+}
